18eaa5ea965e9909e59286e9855d2e2530cbca05
itaekyung <taeyun1411@gmail.com>
2026-03-19 17:06:57 +0900

    카운터 증가 만들기

--- 2주차/week2-self/src/App.tsx
+++ 2주차/week2-self/src/App.tsx
@@ -1,23 +1,12 @@
+import { useState } from "react";
 import "./App.css";
-import List from "./components/List";
 
 function App() {
-  const nickname = "매튜";
-  const sweetPotato = "고구마";
-  const array = ["REACT", "NEXT", "VUE", "SVELTE", "ANGULAR", "REACT-NATIVE"];
+  const [count, setCount] = useState(0);
   return (
     <>
-      <strong className="school">상명대학교</strong>
-      <p style={{ color: "purple", fontWeight: "bold", fontSize: "3rem" }}>
-        {nickname}/김용민
-      </p>
-      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
-      <ul>
-        {array.map((yaho, idx) => (
-          //  <li key={idx}>{yaho}</li>
-          <List key={idx} tech={yaho} />
-        ))}
-      </ul>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>숫자 증가</button>
     </>
   );
 }
